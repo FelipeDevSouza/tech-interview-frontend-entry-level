f60058b5c6067c5a0e190059ad8576f36fee7bba
Felipe de Oliveira Souza <felipe.oliveirac8@gmail.com>
2024-11-27 16:46:44 -0300

    feat: initial commit

---
+++ frontend/src/services/recommendation.service.js
@@ -0,0 +1,13 @@
+// getRecommendations.js
+
+const getRecommendations = (
+  formData = { selectedPreferences: [], selectedFeatures: [] },
+  products
+) => {
+  /**
+   * Crie aqui a lógica para retornar os produtos recomendados.
+   */
+  return [];
+};
+
+export default { getRecommendations };
